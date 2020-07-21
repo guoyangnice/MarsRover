52cd6bd6a6f9c3cfb1ee14247d99305e54580442
yangjie <yangjienice@126.com>
2020-07-21 20:37:39 +0800

    添加初始为WSE方向的测试

--- src/test/java/com/twschool/practice/marsrover/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/marsrover/MarsRoverTest.java
@@ -19,6 +19,39 @@ public class MarsRoverTest {
     }
 
     @Test
+    public void should_return_x_f1_y_0_and_W_when_receive_M_command_give_mars_rover_with_init_place_x_0_y_0_W() {
+        MarsRover marsRover = new MarsRover(new MarsRoverPosition(0 ,0 , "W"));
+
+        MarsRoverPosition marsRoverPosition = marsRover.receive("M");
+
+        assertThat(marsRoverPosition.getCoordinatesX(), is(-1));
+        assertThat(marsRoverPosition.getCoordinatesY(), is(0));
+        assertThat(marsRoverPosition.getDirectionShortName(), is("W"));
+    }
+
+    @Test
+    public void should_return_x_0_y_f1_and_S_when_receive_M_command_give_mars_rover_with_init_place_x_0_y_0_S() {
+        MarsRover marsRover = new MarsRover(new MarsRoverPosition(0 ,0 , "S"));
+
+        MarsRoverPosition marsRoverPosition = marsRover.receive("M");
+
+        assertThat(marsRoverPosition.getCoordinatesX(), is(0));
+        assertThat(marsRoverPosition.getCoordinatesY(), is(-1));
+        assertThat(marsRoverPosition.getDirectionShortName(), is("S"));
+    }
+
+    @Test
+    public void should_return_x_1_y_0_and_E_when_receive_M_command_give_mars_rover_with_init_place_x_0_y_0_E() {
+        MarsRover marsRover = new MarsRover(new MarsRoverPosition(0 ,0 , "E"));
+
+        MarsRoverPosition marsRoverPosition = marsRover.receive("M");
+
+        assertThat(marsRoverPosition.getCoordinatesX(), is(1));
+        assertThat(marsRoverPosition.getCoordinatesY(), is(0));
+        assertThat(marsRoverPosition.getDirectionShortName(), is("E"));
+    }
+
+    @Test
     public void should_return_x_0_y_2_and_N_when_receive_MM_command_give_mars_rover_with_init_place_x_0_y_0_N() {
         MarsRover marsRover = new MarsRover(new MarsRoverPosition(0 ,0 , "N"));
 
